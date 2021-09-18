db19348a5e69da7f567c74781a0753fc084a0fcd
kaye garcia <kayemgarcia@gmail.com>
2021-09-18 13:38:10 -0500

    commit tree ce5e4dcfd5f42bcf035b0cca0a74db90a83845ed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+/* createEmployeeRecord function
+[String, String, String, Number]
+firstName, lastName, title, payRate
+returns Obj with keys:
+firstName
+lastName
+title
+payPerHour
+timeInEvents
+timeOutEvents
+Behavior: loads array elements into corres. object properties
+Initialize empty Arrays on the properties timeInEvents and timeOutEvents
+*/
+
+
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
+  return testEmployeeObj[0];
+}
+
+function createEmployeeRecords(){
+
+}
